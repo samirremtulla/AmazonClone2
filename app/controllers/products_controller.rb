5c55fd518e68d4e6f2f6a8a034edd6f1805568f3
Samir Remtulla <samir.remtulla@gmail.com>
2013-03-27 08:47:08 -0400

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,31 @@
+class ProductsController < ApplicationController
+  
+  def index
+  #displays all products
+  end
+
+  def show
+  #displays individual products
+  end
+
+  def new
+  #displays form for new product
+  end
+
+  def create
+  #creates new product
+  end
+
+  def edit
+  #edits a product
+  end
+  
+  def update
+  #submits edit to a product
+  end
+
+  def destroy
+  #deletes a product
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = '13fa996c0b2f0b78e72d7e0e0a3262a9c4d38f2081fac318291431cd3319674afea38c62713699bdbcfd9b70ace957e13108e099fa244038966d90f3e4b21e83'
